324103f3f1d8bce30a9e302c9fd6e11c3a1ead6d
Viraj Solanki <virazssolanki@gmail.com>
2021-08-21 12:15:45 +0530

    chnages

--- main/models.py
+++ main/models.py
@@ -4,21 +4,17 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class Merged(models.Model):
     m_img = models.ImageField(default='d.png', upload_to='merged_pic')
     name = models.CharField(max_length=50, blank=False, default='')
-    village = models.CharField(max_length=50, blank=False, default='')
     number = models.CharField(max_length=10, blank=False, default='')
 
     def __str__(self):
         return f'{self.id} merged'
 
-O_CHOICES = (
-    ('square','square'),
-    ('landscape', 'landscape'),
-)
 
 class Frame(models.Model):
     frame = models.ImageField(default='d.png', upload_to='frames_pic')
     mask = models.ImageField(default='d.png', upload_to='mask_img')
-    orientation = models.CharField(max_length=30, default='', choices=O_CHOICES)
+    frame_heading = models.CharField(max_length=150, blank=False, default='')
+    frame_desc = models.CharField(max_length=350, blank=False, default='')
     def __str__(self):
         return f'{self.id} frame'
 
